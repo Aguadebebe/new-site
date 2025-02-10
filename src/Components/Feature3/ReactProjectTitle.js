55096baef9b0af4ab46f1fc58f6791f673e1782a
Aguadebebe <81484212+Aguadebebe@users.noreply.github.com>
2025-02-10 08:43:50 -0500

    equal sign changed

--- src/Components/Feature3/ReactProjectTitle.js
+++ src/Components/Feature3/ReactProjectTitle.js
@@ -6,7 +6,7 @@ const ReactProjectTitle = () => {
     return (
         <span>     
           <h1 className="react-project-title">React Project Apps</h1>
-          <span>{equal}</span>
+           {equal}
         </span>
     );
 }
