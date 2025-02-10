9bdf5f9e88f7ecee3b30c3a6c4f83d001c221622
Aguadebebe <81484212+Aguadebebe@users.noreply.github.com>
2025-02-10 09:15:37 -0500

    corrected equal sign issue

--- src/Components/Feature3/ReactProjectTitle.js
+++ src/Components/Feature3/ReactProjectTitle.js
@@ -2,9 +2,9 @@ import "./ReactProjectTitle.css";
 const ReactProjectTitle = () => {
  
     return (
-        <span>     
+        <div>     
           <h1 className="react-project-title">React Project Apps =</h1>
-        </span>
+        </div>
     );
 }
 
