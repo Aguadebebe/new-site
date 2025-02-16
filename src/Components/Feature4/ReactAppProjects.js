4b2ed41c7cbe0c8e986d2b08a48a7830b99347b9
Aguadebebe <81484212+Aguadebebe@users.noreply.github.com>
2025-02-16 13:33:06 -0500

    Added repo url's for projects

--- src/Components/Feature4/ReactAppProjects.js
+++ src/Components/Feature4/ReactAppProjects.js
@@ -1,34 +1,37 @@
 import "./ReactAppProjects.css";
 
 const ReactAppProjects = () => {
+  const apps = [
+    {
+      title: "Color-Picks,",
+      url: "https://aguadebebe.github.io/color-picks/"
+    },
 
-    const apps = [
-      { 
-        title: "Color-Picks,", 
-        url: "https://aguadebebe.github.io/color-picks/", 
-      },
-      
-      { 
-        title: "Todo-List,", 
-        url: "https://aguadebebe.github.io/todo-list/",
-      },
-    ];
+    {
+      title: "Color-picks-GitHub Repository",
+      url: "https://github.com/Aguadebebe/color-picks"
+    },
 
-    return (
-        
-        <div className="map-container">
-           {apps.map((app, index) => (
-              <a 
-                key={index}
-                href={app.url}
-                target={"_blank"}
-                rel="noopener noreferrer"
-              >
-                <div className="app-title">{app.title}</div>
-              </a>
-          ))}
-        </div>
-    );
-} 
+    {
+      title: "Todo-List,",
+      url: "https://aguadebebe.github.io/todo-list/"
+    },
+
+    {
+      title: "Todo-List-GitHub Repository",
+      url: "https://github.com/Aguadebebe/todo-list"
+    }
+  ];
+
+  return (
+    <div className="map-container">
+      {apps.map((app, index) => (
+        <a key={index} href={app.url} target={"_blank"} rel="noopener noreferrer">
+          <div className="app-title">{app.title}</div>
+        </a>
+      ))}
+    </div>
+  );
+};
 
 export default ReactAppProjects;
